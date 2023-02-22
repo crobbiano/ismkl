93da494e1c00f635f8dc28f94a8473a251983971
chris <cp.robbiano@gmail.com>
2023-02-22 02:59:04 -0700

    IS-2 initial work

--- src/kernel_matrix.py
+++ src/kernel_matrix.py
@@ -39,7 +39,7 @@ class KernelMatrix:
                 case 'linear':
                     tmp = (np.dot(self.X.T, self.Y) + v['param1'])
                 case 'tanh':
-                    pass
+                    tmp = np.tanh(v['param1'] + v['param2'] * np.dot(self.X.T, self.Y))
                 case _:
                     raise NotImplementedError(f'Kernel type {k} does not match any of the supported kernel types:'
                                               f' {self.supported_kernels}')
@@ -50,9 +50,14 @@ class KernelMatrix:
 
 
 if __name__ == '__main__':
-    X = np.random.random((10,5))
-    Y = np.random.random((10,2))
+    X = np.random.random((1000, 50))
+    Y = np.random.random((1000, 28))
     kernels = {'gaussian': {'param1': 1},
-               'linear': {'param1': 1}}
+               'linear': {'param1': 1},
+               'polynomial': {'param1': 1, 'param2': 1},
+               'tanh': {'param1': 1, 'param2': 1},
+               'quartic': {'param1': 1},
+               }
     km = KernelMatrix(X, Y, kernels)
+    print((km.matrix.shape))
     print(km.matrix)
