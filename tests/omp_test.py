536b17298f80df2e7f4b61e8c2dddfdb55cd5494
chris <cp.robbiano@gmail.com>
2023-02-22 12:30:26 -0700

    IS-4 OMP working

---
+++ tests/omp_test.py
@@ -0,0 +1,24 @@
+import unittest
+import numpy as np
+from src.kernel_matrix import KernelMatrix
+from src.recursive_omp import RecursiveOMP
+
+
+class RecursiveOMPTest(unittest.TestCase):
+
+    def test_omp_runs(self):
+        # Arrange
+        num_x, num_y, N = 50, 28, 1000
+        features = np.random.random((N, num_x))
+        kernel_dict = {
+            'gaussian': {'param1': 1},
+            'quartic': {'param1': 1},
+        }
+        values = np.random.randint(0, 4, num_x)
+        n_values = np.max(values) + 1
+        L0 = np.eye(n_values)[values]
+        K00 = KernelMatrix(features, features, kernel_dict).matrix
+        # Act
+        W0 = RecursiveOMP(K00, [], L0, residual_norm=0.1).run()
+        # Assert
+        self.assertEqual(W0.shape, (100, 4))
